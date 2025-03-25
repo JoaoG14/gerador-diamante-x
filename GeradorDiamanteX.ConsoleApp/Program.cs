ec1d6d729c63b10399aeb441febf8d2df38b9955
João <joaog.alves14@gmail.com>
2025-03-25 16:44:07 -0300

    Estrutura inicial do programa

--- GeradorDiamanteX.ConsoleApp/Program.cs
+++ GeradorDiamanteX.ConsoleApp/Program.cs
@@ -4,7 +4,77 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("=========================");
+            Console.WriteLine("Gerador de Diamante com X");
+            Console.WriteLine("=========================");
+
+            int numero = 0;
+            bool entradaValida = false;
+
+            while (!entradaValida)
+            {
+                Console.Write("\nDigite um número ímpar: ");
+                
+                if (int.TryParse(Console.ReadLine(), out numero))
+                {
+                    if (numero % 2 != 0)
+                    {
+                        entradaValida = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Entrada inválida! O número deve ser ímpar. Tente novamente.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida! Digite um número inteiro.");
+                }
+            }
+
+            int espacos = tamanho / 2;
+            int xs = 1;
+
+            for (int i = 0; i <= tamanho / 2; i++)
+            {
+                for (int j = 0; j < espacos; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < xs; j++)
+                {
+                    Console.Write("X");
+                }
+
+                Console.WriteLine();
+                espacos--;
+                xs += 2;
+            }
+
+            espacos = 1;
+            xs = tamanho - 2;
+
+            for (int i = 0; i < tamanho / 2; i++)
+            {
+                for (int j = 0; j < espacos; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < xs; j++)
+                {
+                    Console.Write("X");
+                }
+
+                Console.WriteLine();
+                espacos++;
+                xs -= 2;
+            }
+
+            Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
         }
+
     }
 }
